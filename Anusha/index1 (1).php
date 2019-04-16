a938d067abd8ea9753d7943f21347d531f7ae03a
nischala13 <47168404+nischala13@users.noreply.github.com>
2019-04-16 11:34:49 +0530

    Add files via upload

---
+++ Anusha/index1 (1).php
@@ -0,0 +1,36 @@
+﻿<?php
+include('connnew.php');
+$query="select Q_no,Q_txt,Opt1,Opt2,Opt3,Opt4,ans from questiom";
+$result=mysqli_query($db,$query);
+?>
+<!DOCTYPE html>
+<html>
+<title>
+<head>question</head>
+</title>
+<body>
+
+	<tr>
+	<th><h2>question</h2></th>
+	</tr>
+
+<?php
+	while($rows=mysqli_fetch_assoc($result))
+	{
+?>	
+	
+	<?php echo $rows['Q_no'],'.  ',$rows['Q_txt'];?><br></br>
+<table>
+	<input type="radio" name="<?php echo $rows['Q_no']?>" value="1"><td><tr><?php echo 'A.  ';?></tr><?php echo $rows['Opt1'];?></td><br></br>
+	<input type="radio" name="<?php echo $rows['Q_no']?>" value="2"><td><tr><?php echo 'B.  ';?></tr><?php echo $rows['Opt2'];?></td><br></br>
+	<input type="radio" name="<?php echo $rows['Q_no']?>" value="3"><td><tr><?php echo 'C.  ';?></tr><?php echo $rows['Opt3'];?></td><br></br>
+	<input type="radio" name="<?php echo $rows['Q_no']?>" value="4"><td><tr><?php echo 'D.  ';?></tr><?php echo $rows['Opt4'];?></td><br></br>
+
+
+</table>
+<?php
+	}
+?>
+</body>
+</html>
+	
---
+++ Anusha/online11.php
@@ -0,0 +1,61 @@
+﻿<html>
+<head>
+<style>
+table,td 
+{border:1px solid black;
+border-collpase:collapse;}
+td{
+padding:5px;}
+</style>
+<titlt>QUIZ</title>
+</head>
+<body style="background-color:white;">
+<h1 style="text-align center;"><b>TEST 1</b></h1>
+<hr>
+<p>
+
+<form action="" method="post" style="text-align:center:font-size:150%">
+
+<td>question1:</td><td><input type="varchar" name="q"></td></tr>
+<tr><br>
+<form><div id=group1>
+  <input type="radio" name="group1" value="1">a <br>
+  <input type="radio" name="group1" value="2">b <br>
+  <input type="radio" name="group1" value="3">c <br>
+ <input type="radio" name="group1" value="4">d <br>
+</div>
+
+</form>
+<td>answer:</td><td><input type="text" name="a"></td><tr>
+<tr><br>
+
+<br><td>question2:</td><td><input type="varchar" name="q"></td></tr>
+<tr><br>
+<form><div id=group2>
+ <input type="radio" name="group2" value="1">a <br>
+  <input type="radio" name="group2" value="2">b <br>
+  <input type="radio" name="group2" value="3">c <br>
+ <input type="radio" name="group2" value="4">d <br>
+</form>
+<td>answer:</td><td><input type="text" name="a"></td><tr>
+<tr><br>
+<br><td>question3:</td><td><input type="varchar" name="q"></td></tr>
+<tr><br>
+<form><div id=group3>
+ <input type="radio" name="group3" value="1">a <br>
+  <input type="radio" name="group3" value="2">b <br>
+  <input type="radio" name="group3" value="3">c <br>
+ <input type="radio" name="group3" value="4">d <br>
+</form>
+<br><td>answer:</td><td><input type="text" name="a"></td><tr>
+<tr><br>
+<br><td>coding question 1</td><td><input type="varchar" name="q"></td></tr>
+<tr><br>
+<p>code:</p>
+<td></td><td><textarea rows="20" cols="100"></textarea></td><tr>
+<tr><br>
+<br>
+</form>
+</p>
+</body>
+`</html>
